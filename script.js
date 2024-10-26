74cf3b5dfdcefd66f3d1d453f731f28e7c832734
heetjain09 <heet.236057101@vcet.edu.in>
2024-10-26 01:35:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,238 +1,100 @@
-let country_list = {
-  AED: "AE",
-  AFN: "AF",
-  XCD: "AG",
-  ALL: "AL",
-  AMD: "AM",
-  ANG: "AN",
-  AOA: "AO",
-  AQD: "AQ",
-  ARS: "AR",
-  AUD: "AU",
-  AZN: "AZ",
-  BAM: "BA",
-  BBD: "BB",
-  BDT: "BD",
-  XOF: "BE",
-  BGN: "BG",
-  BHD: "BH",
-  BIF: "BI",
-  BMD: "BM",
-  BND: "BN",
-  BOB: "BO",
-  BRL: "BR",
-  BSD: "BS",
-  NOK: "BV",
-  BWP: "BW",
-  BYR: "BY",
-  BZD: "BZ",
-  CAD: "CA",
-  CDF: "CD",
-  XAF: "CF",
-  CHF: "CH",
-  CLP: "CL",
-  CNY: "CN",
-  COP: "CO",
-  CRC: "CR",
-  CUP: "CU",
-  CVE: "CV",
-  CYP: "CY",
-  CZK: "CZ",
-  DJF: "DJ",
-  DKK: "DK",
-  DOP: "DO",
-  DZD: "DZ",
-  ECS: "EC",
-  EEK: "EE",
-  EGP: "EG",
-  ETB: "ET",
-  EUR: "FR",
-  FJD: "FJ",
-  FKP: "FK",
-  GBP: "GB",
-  GEL: "GE",
-  GGP: "GG",
-  GHS: "GH",
-  GIP: "GI",
-  GMD: "GM",
-  GNF: "GN",
-  GTQ: "GT",
-  GYD: "GY",
-  HKD: "HK",
-  HNL: "HN",
-  HRK: "HR",
-  HTG: "HT",
-  HUF: "HU",
-  IDR: "ID",
-  ILS: "IL",
-  INR: "IN",
-  IQD: "IQ",
-  IRR: "IR",
-  ISK: "IS",
-  JMD: "JM",
-  JOD: "JO",
-  JPY: "JP",
-  KES: "KE",
-  KGS: "KG",
-  KHR: "KH",
-  KMF: "KM",
-  KPW: "KP",
-  KRW: "KR",
-  KWD: "KW",
-  KYD: "KY",
-  KZT: "KZ",
-  LAK: "LA",
-  LBP: "LB",
-  LKR: "LK",
-  LRD: "LR",
-  LSL: "LS",
-  LTL: "LT",
-  LVL: "LV",
-  LYD: "LY",
-  MAD: "MA",
-  MDL: "MD",
-  MGA: "MG",
-  MKD: "MK",
-  MMK: "MM",
-  MNT: "MN",
-  MOP: "MO",
-  MRO: "MR",
-  MTL: "MT",
-  MUR: "MU",
-  MVR: "MV",
-  MWK: "MW",
-  MXN: "MX",
-  MYR: "MY",
-  MZN: "MZ",
-  NAD: "NA",
-  XPF: "NC",
-  NGN: "NG",
-  NIO: "NI",
-  NPR: "NP",
-  NZD: "NZ",
-  OMR: "OM",
-  PAB: "PA",
-  PEN: "PE",
-  PGK: "PG",
-  PHP: "PH",
-  PKR: "PK",
-  PLN: "PL",
-  PYG: "PY",
-  QAR: "QA",
-  RON: "RO",
-  RSD: "RS",
-  RUB: "RU",
-  RWF: "RW",
-  SAR: "SA",
-  SBD: "SB",
-  SCR: "SC",
-  SDG: "SD",
-  SEK: "SE",
-  SGD: "SG",
-  SKK: "SK",
-  SLL: "SL",
-  SOS: "SO",
-  SRD: "SR",
-  STD: "ST",
-  SVC: "SV",
-  SYP: "SY",
-  SZL: "SZ",
-  THB: "TH",
-  TJS: "TJ",
-  TMT: "TM",
-  TND: "TN",
-  TOP: "TO",
-  TRY: "TR",
-  TTD: "TT",
-  TWD: "TW",
-  TZS: "TZ",
-  UAH: "UA",
-  UGX: "UG",
-  USD: "US",
-  UYU: "UY",
-  UZS: "UZ",
-  VEF: "VE",
-  VND: "VN",
-  VUV: "VU",
-  YER: "YE",
-  ZAR: "ZA",
-  ZMK: "ZM",
-  ZWD: "ZW"
-};
+document.getElementById('expense-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('expense-name').value;
+    const amount = document.getElementById('expense-amount').value;
+    const category = document.getElementById('expense-category').value;
 
-let apiKey = "e759f92560e41c99ee6213a2";
-
-const dropList = document.querySelectorAll("form select"),
-  fromCurrency = document.querySelector(".from select"),
-  toCurrency = document.querySelector(".to select"),
-  getButton = document.querySelector("form button");
-
-for (let i = 0; i < dropList.length; i++) {
-  for (let currency_code in country_list) {
-    let selected =
-      i == 0
-        ? currency_code == "USD"
-          ? "selected"
-          : ""
-        : currency_code == "INR"
-        ? "selected"
-        : "";
-    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-    dropList[i].insertAdjacentHTML("beforeend", optionTag);
-  }
-  dropList[i].addEventListener("change", (e) => {
-    loadFlag(e.target);
-  });
-}
-
-function loadFlag(element) {
-  for (let code in country_list) {
-    if (code == element.value) {
-      let imgTag = element.parentElement.querySelector("img");
-      imgTag.src = `https://flagcdn.com/48x36/${country_list[
-        code
-      ].toLowerCase()}.png`;
-    }
-  }
-}
-
-window.addEventListener("load", () => {
-  getExchangeRate();
-});
-
-getButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  getExchangeRate();
-});
-
-const exchangeIcon = document.querySelector("form .icon");
-exchangeIcon.addEventListener("click", () => {
-  let tempCode = fromCurrency.value;
-  fromCurrency.value = toCurrency.value;
-  toCurrency.value = tempCode;
-  loadFlag(fromCurrency);
-  loadFlag(toCurrency);
-  getExchangeRate();
-});
-
-function getExchangeRate() {
-  const amount = document.querySelector("form input");
-  const exchangeRateTxt = document.querySelector("form .exchange-rate");
-  let amountVal = amount.value;
-  if (amountVal == "" || amountVal == "0") {
-    amount.value = "1";
-    amountVal = 1;
-  }
-  exchangeRateTxt.innerText = "Getting exchange rate...";
-  let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((result) => {
-      let exchangeRate = result.conversion_rates[toCurrency.value];
-      let totalExRate = (amountVal * exchangeRate).toFixed(2);
-      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    fetch('/add_expense', {
+        method: 'POST',
+        body: new URLSearchParams({
+            'name': name,
+            'amount': amount,
+            'category': category
+        }),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
     })
-    .catch(() => {
-      exchangeRateTxt.innerText = "Something went wrong";
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            loadExpenses();
+        }
+    });
+});
+
+function loadExpenses() {
+    fetch('/api/expenses')
+        .then(response => response.json())
+        .then(expenses => {
+            const expenseList = document.getElementById('expense-list');
+            expenseList.innerHTML = '';
+            let categoryTotals = {};
+
+            expenses.forEach(expense => {
+                const li = document.createElement('li');
+                li.innerHTML = `${expense.name}: ₹${expense.amount} (${expense.category}) 
+                <button onclick="deleteExpense(${expense.id})">Delete</button>`;
+                expenseList.appendChild(li);
+
+                // Calculate category totals for pie chart
+                if (!categoryTotals[expense.category]) {
+                    categoryTotals[expense.category] = 0;
+                }
+                categoryTotals[expense.category] += expense.amount;
+            });
+
+            drawPieChart(categoryTotals);
+        });
+}
+
+function deleteExpense(id) {
+    fetch(`/delete_expense/${id}`, { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                loadExpenses();
+            }
+        });
+}
+
+function drawPieChart(data) {
+    const ctx = document.getElementById('expenseChart').getContext('2d');
+    const labels = Object.keys(data);
+    const values = Object.values(data);
+
+    if (window.pieChart) {
+        window.pieChart.destroy();
+    }
+
+    window.pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Expense Categories',
+                data: values,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)'
+                ]
+            }]
+        },
+        options: {
+            responsive: true
+        }
     });
 }
+
+function logout() {
+    fetch('/logout').then(() => {
+        window.location.href = '/';
+    });
+}
+
+// Load expenses when the page loads
+window.onload = loadExpenses;
